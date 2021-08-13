948cd670b467361eaaaceb5955d9c3d2cffa6670
ibrahim yurtseven <hiyurt7@gmail.com>
2021-08-13 02:27:31 -0700

    third commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -6,6 +6,6 @@ public class ClassOne {
     public static void main(String[] args) {
  System.out.println("Hello World");
         System.out.println("Batch 35 / 36 learn Github ");
-
+        System.out.println("We like Git but we will like more when we have practice ");
     }
 }
